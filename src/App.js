6ff71ec5820a834213a2fdb5cb7e20f1169685da
Sol Youn <syoun93@gmail.com>
2022-03-20 15:08:34 -0700

    created and tested routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Home from "./components/pages/Home"
+import Blog from "./components/pages/Blog"
+import Blogs from "./components/pages/Blogs"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 function App() {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(process.env.REACT_APP_SERVER_URL + "/blogs")
+      .then((response) => {
+        setBlogs(response.data)
+        // console.log(response.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/blogs" element={<Blogs blogs={blogs}/>} />
+          <Route path="/blogs/:id" element={<Blog blogs={blogs}/>} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
